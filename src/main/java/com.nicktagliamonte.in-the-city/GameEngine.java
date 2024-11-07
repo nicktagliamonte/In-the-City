a056b3af9ae5eab2cf817147b165787456627f11
nicktagliamonte <129461012+nicktagliamonte@users.noreply.github.com>
2024-11-07 15:54:16 -0500

    Initial File Structure pushed first lines of code for game engine

---
+++ src/main/java/com.nicktagliamonte.in-the-city/GameEngine.java
@@ -0,0 +1,10 @@
+public class GameEngine {
+    public void startGame() {
+        boolean isRunning = true;
+        while (isRunning) {
+            // capture and process user input
+            //update game state
+            //render or print outputs
+        }
+    }
+}
