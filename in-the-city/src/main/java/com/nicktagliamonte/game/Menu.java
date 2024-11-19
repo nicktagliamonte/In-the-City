71552b40a77f82eebab91bc21928f14e7d5fbdb3
nicktagliamonte <129461012+nicktagliamonte@users.noreply.github.com>
2024-11-19 10:01:15 -0500

    Expanded game manual

--- in-the-city/src/main/java/com/nicktagliamonte/game/Menu.java
+++ in-the-city/src/main/java/com/nicktagliamonte/game/Menu.java
@@ -71,46 +71,32 @@ public class Menu {
     }
 
     private void displayManual() {
-        //think about creating a manual class and having some options.
-        //like a description of commands, a description of items/traps, more if you think of more
-        System.out.println("Game Manual:");
-        System.out.println("No command is case sensitive.  Caps are used below to indicate which words are commands and arguments.");
-        System.out.println("LOOK, look, and LoOk are all valid.");
-        System.out.println("This game is long and is meant to be somewhat adversarial towards you, the player.");
-        System.out.println("But there is a walkthrough available on GitHub if you're frustrated, or more interested in the story than the gameplay.");
-        System.out.println("========================================");
-        System.out.println(" ___________________");
-        System.out.println("|    ORIENTATION    |");
-        System.out.println("|___________________|");
-        System.out.println("   -LOOK: Prints a description of your surroundings, as well as the items, characters, and pathways accessible to you.");
-        System.out.println("   -LOCATE: Describes your current location and gives directions to the nearest safe zone and economic zone.");
-        System.out.println("   -MENU: You used this command to get to this menu.");
-        System.out.println(" ___________________");
-        System.out.println("| MOVEMENT COMMANDS |");
-        System.out.println("|___________________|");
-        System.out.println("   -MOVE [DIRECTION]: Moves 1 step in the direction specified by [DIRECTION].\n\tValid DIRECTIONS are UP, NORTH, DOWN, SOUTH, LEFT, WEST, RIGHT, EAST.");
-        System.out.println("   -MOVE [DIRECTION] [NUMBER OF STEPS]: Moves [NUMBER OF STEPS] steps in the direction specified by [DIRECTION].\n\tValid DIRECTIONS are UP, NORTH, DOWN, SOUTH, LEFT, WEST, RIGHT, EAST.\n\tValid NUMBERS OF STEPS are integers greater than or equal to 1.");
-        System.out.println("   -MOVE TO [WAYPOINT]: Moves to the WAYPOINT specified by [WAYPOINT].\n\tValid WAYPOINTS are the names of characters, items, or pathways you currently have access to.\n\tUse LOOK for a list of such waypoints.");
-        System.out.println("   -ENTER [ROOM NAME]: Moves immediately to the room specified by [ROOM NAME], if it adjoins your current location.\n\tCurrently available ROOM NAMES can be found using the LOOK command.");
-        System.out.println("   -ASCEND [ROOM NAME]: Moves immediately to the room specified by [ROOM NAME], if it adjoins your current location and is accessed via staircase going up.\n\tCurrently available ROOM NAMES can be found using the LOOK command.");
-        System.out.println("   -DESCEND [ROOM NAME]: Moves immediately to the room specified by [ROOM NAME], if it adjoins your current location and is accessed via staircase going down.\n\tCurrently available ROOM NAMES can be found using the LOOK command.");
-        System.out.println(" _____________________");
-        System.out.println("| OBJECT INTERACTIONS |");
-        System.out.println("|_____________________|");
-        System.out.println("   EXAMINE [ITEM NAME]: Gives a more detailed description of the item specified by [ITEM NAME]\n\tUse LOOK and/or INVENTORY to get a list of available items.");
-        System.out.println("   TAKE [ITEM NAME]: Adds [ITEM NAME] to the player/party inventory, if there is room.");
-        System.out.println("   USE [ITEM NAME]: Uses [ITEM NAME], if it has a use.");
-        System.out.println("   DROP [ITEM NAME]: Removes [ITEM NAME] from inventory, if there is an item of such name in inventory");
-        System.out.println(" ________________________");
-        System.out.println("| CHARACTER INTERACTIONS |");
-        System.out.println("|________________________|");
-        System.out.println("   TALK TO [CHARACTER NAME]: Enters Dialogue with [CHARACTER NAME].");
-        System.out.println("   JOIN [CHARACTER NAME]: Adds [CHARACTER NAME] to the party, if there is not currently a party member of the same class as [CHARACTER NAME].\n\tThis can only be undone if [CHARACTER NAME] dies.");
-        System.out.println("   FIGHT [CHARACTER NAME]: Enters Combat with [CHARACTER NAME].");
-        System.out.println("   HINT [CHARACTER NAME]: Gets a hint from [CHARACTER NAME], if they have one to offer and you have one to receive.");
-        System.out.println("   HIDE: Attempts to hide or conceal the presence of the party, reducing detectability and the chance of unwanted interactions.");
-        System.out.println("========================================");
-        System.out.println("\nEnter 1 to return to the main menu.");
+        System.out.println("1. Display a list of commands available in the game.");
+        System.out.println("2. Display a description of the various items available in the game.");
+        System.out.println("3. Return to the main menu.");
+        Manual manual = new Manual();
+        while (true) {
+            int input = gameEngine.getPlayerInputAsInt();
+            if (input == 1) {
+                manual.printCommands();
+                System.out.println("");
+                System.out.println("1. Display a list of commands available in the game.");
+                System.out.println("2. Display a description of the various items available in the game.");
+                System.out.println("3. Return to the main menu.");
+            } else if (input == 2) {
+                manual.printItems();
+                System.out.println("");
+                System.out.println("1. Display a list of commands available in the game.");
+                System.out.println("2. Display a description of the various items available in the game.");
+                System.out.println("3. Return to the main menu.");
+            } else if (input == 3) {
+                displayMenu();
+                break;
+            }
+            else {
+                System.out.println("Invalid choice. Enter 3 to return to the main menu.");
+            }
+        }
     
         while (true) {
             int input = gameEngine.getPlayerInputAsInt();
