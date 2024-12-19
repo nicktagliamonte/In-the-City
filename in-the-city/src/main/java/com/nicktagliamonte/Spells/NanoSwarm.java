70d4901fa6156f48e78b1e3eb107ab5bae1346bb
nicktagliamonte <129461012+nicktagliamonte@users.noreply.github.com>
2024-12-19 17:48:28 -0500

    Created remaining spells

---
+++ in-the-city/src/main/java/com/nicktagliamonte/Spells/NanoSwarm.java
@@ -0,0 +1,45 @@
+package com.nicktagliamonte.Spells;
+
+import java.util.Random;
+
+import com.google.gson.annotations.Expose;
+import com.nicktagliamonte.characters.Adversary;
+import com.nicktagliamonte.characters.Player;
+
+public class NanoSwarm extends Spell {
+    @Expose private int dieQuantity = 1;
+    @Expose private int dieFaces = 4;
+
+    public NanoSwarm() {
+        super("Nano Swarm", "Releases a swarm of nanobots that attack a single enemy, tearing and corroding them.", "4d8");
+    }
+
+    @Override
+    public void cast(Player caster, Adversary adversary) {
+        int baseDamage = new Random().ints(dieQuantity, 1, dieFaces + 1).sum();
+        int strengthBonus = (int) (Math.random() * caster.getStrength()) + 1;
+        int totalDamage = baseDamage + strengthBonus;
+
+        // Apply hunger penalty if the caster is in hunger
+        if (caster.inHunger()) {
+            totalDamage = Math.max(totalDamage - caster.getLevel(), 1);
+        }
+
+        // Inflict damage on the adversary
+        adversary.takeDamage(totalDamage);
+
+        // Notify the user
+        pauseForEffect();
+        System.out.println(caster.getName() + " casts " + getName() + " and deals " 
+            + totalDamage + " damage to " + adversary.getName() + "!");
+    }
+
+    // Helper method to handle the delay
+    private void pauseForEffect() {
+        try {
+            Thread.sleep(15);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
